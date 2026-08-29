62d5e69274dd3315bb8dc05e873f73b3c5b62951
Stephan Malobeka <malozdev@gmail.com>
2026-08-29 12:28:35 +0200

    fix dockerfile and next.config for standalone build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ['paging-undermost-ultra.ngrok-free.dev'],
   images: {
     remotePatterns: [
